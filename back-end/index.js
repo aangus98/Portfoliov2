2ffa2cc5e6b36289bc4ab2645770f6ddcc6eb798
Andrew <mrangus298@gmail.com>
2025-04-17 11:36:56 -0500

    Added a basic backend

--- back-end/index.js
+++ back-end/index.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Portfolio backend is running.');
-});
+    res.status(200).json({ status: 'OK', message: 'Portfolio backend is running.' });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
